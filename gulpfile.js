5ea9ce9785673b7eb09eab941939b46c2e878e9d
jderus <jderus@gmail.com>
2016-02-13 13:41:40 -0800

    First!

---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+﻿var gulp = require("gulp"),
+    concat = require("gulp-concat"),
+    cssmin = require("gulp-cssmin"),
+    uglify = require("gulp-uglify");
+
+var plugins = require('gulp-load-plugins')();
+
