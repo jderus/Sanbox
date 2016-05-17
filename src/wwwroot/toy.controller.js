7bbf8e7761b28a0908b8220c7e57758adf1ed07f
jayd <jayd@microsoft.com>
2016-05-16 21:13:31 -0700

    d3 bar start

--- src/wwwroot/toy.controller.js
+++ src/wwwroot/toy.controller.js
@@ -29,7 +29,9 @@
         function activate() {
             vm.title = "Sandbox";
             vm.text  = ""
+            
             drawStuff();
+            drawBarStuff();
             
             fiddleService.getData().$promise.then(
                 function (value) { 
@@ -56,6 +58,37 @@
             circles.attr("cx", function(d, i) { return (i * 50) + 25; })
                    .attr("cy", h/2)
                    .attr("r", function(d) { return d; });
+            
+        }
+        
+        function drawBarStuff() {
+            var data = [4, 8, 15, 16, 23, 42];
+
+            var width = 420,
+                barHeight = 20;
+
+            var x = d3.scale.linear()
+                .domain([0, d3.max(data)])
+                .range([0, width]);
+
+            var chart = d3.select(".chart")
+                .attr("width", width)
+                .attr("height", barHeight * data.length);
+
+            var bar = chart.selectAll("g")
+                .data(data)
+            .enter().append("g")
+                .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+
+            bar.append("rect")
+                .attr("width", x)
+                .attr("height", barHeight - 1);
+
+            bar.append("text")
+                .attr("x", function(d) { return x(d) - 3; })
+                .attr("y", barHeight / 2)
+                .attr("dy", ".35em")
+                .text(function(d) { return d; });
             
         }
         
